4e16501c18a1e01d6d17e834ad2d1096b5a44667
merryful <merrynguyen9@yahoo.com>
2023-01-27 14:34:03 -0500

    add changes

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const navbar = document.getElementById("navbar");
 const body = document.body;
 var sticky = navbar.offsetTop;
 
-window.addEventListener('resize', checkScreenSize);
-
 window.addEventListener('scroll', function(e) {
     const currentScroll = window.pageYOffset;
 
@@ -13,14 +11,12 @@ window.addEventListener('scroll', function(e) {
 		body.classList.add("hide");
         navbar.classList.remove("sticky");
 	}
-
     if (currentScroll >= sticky) {
         navbar.classList.add("sticky");
         body.classList.remove("hide");
     }
 
 });
-
 
 document.addEventListener("DOMContentLoaded", function() {
     var icon2 = document.getElementById("icon2");
